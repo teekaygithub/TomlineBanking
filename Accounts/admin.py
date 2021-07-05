168db722cc0f9343435998aae4dc3a5e48a1ca4e
teekaygithub <tomoyuki.kato@yahoo.com>
2021-07-05 12:33:13 -0700

    Changed account primary key to be a 16-byte UUID. Changed account type field to be a set choice between checking, savings, and credit-card.

--- Accounts/admin.py
+++ Accounts/admin.py
@@ -2,7 +2,7 @@ from Accounts.models import Account
 from django.contrib import admin
 
 class AccountAdmin(admin.ModelAdmin):
-    list_display= ("description", "is_active", "user")
+    list_display= ("id", "user", "type", "date_created", "is_active",)
 
 # Register your models here.
 admin.site.register(Account, AccountAdmin)
--- Accounts/models.py
+++ Accounts/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
 
 # Create your models here.
 class Account(models.Model):
+    ACCOUNT_TYPES = (
+        ('CH', 'Checking'),
+        ('SA', 'Saving'),
+        ('CC', 'Credit Card'),
+    )
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    description = models.CharField(default="Checking", max_length=100)
+    type = models.CharField(default="Checking", max_length=100, choices=ACCOUNT_TYPES)
     date_created = models.DateTimeField(auto_now=True)
     date_deactivated = models.DateTimeField(blank=True, null=True)
     balance = models.IntegerField(default=1000)
